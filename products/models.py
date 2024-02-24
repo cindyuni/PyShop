a0dd8b8676220067ab0d6a23f40777160577bc31
bigbang0818513 <bigbang0818513@gmail.com>
2024-02-24 21:01:57 +0800

    Add products to the shop

--- products/models.py
+++ products/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=255)  # CharField是可以接受文字類型的輸入
+    name = models.CharField(max_length=255)  # CharField a filed that can contain textual data
     price = models.FloatField()  # FloatField是可以接受float類型的輸入
     stock = models.IntegerField()
     image_url = models.CharField(max_length=2083)
--- products/views.py
+++ products/views.py
@@ -1,12 +1,15 @@
 # views 是使用者可以看到些什麼,在哪一頁可以看到什麼
-# 每一個views裡面的function都要有吃request這個parameter
+# 每一個views裡面的function都要有吃 request這個parameter
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product
 
 
 # /products -> index -> main page
 def index(request):
-    return HttpResponse('Hello World')
+    products = Product.objects.all()
+    return render(request, 'index.html',
+                  {'products' : products})
 
 
 # products/new
